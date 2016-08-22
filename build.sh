7085cbb16ee1d63530c92f295502222f848685f7
Viktor A. Manyushin <vmanyushin@gmail.com>
2016-08-22 08:44:58 -0700

    файл build.sh не нужен в пакетах

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ cd /opt && git clone https://github.com/vmanyushin/raid-tools
 rm -rf /opt/raid-tools/.git
 rm -rf /opt/raid-tools/packages
 rm /opt/raid-tools/.gitignore
+rm /opt/raid-tools/build.sh
 
 rm -rf /home/sysop/development/packages/*
 fpm -s dir -t deb -v 0.3 -a all -n raid-tools --depends wget --depends gawk --depends pciutils -p /home/sysop/development/packages/ /opt/raid-tools
